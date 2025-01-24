e629faf7347f8924a6e5f9cc4ee8876efc019bf2
dohauzi@gmail.com <dohauzi@gmail.com>
2025-01-24 14:10:47 +0900

    컴포넌트 파일 jsx로 변경

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
             }
         ]
     },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
     devServer: {
         static: {
             directory: path.join(__dirname, 'public')
