5e0a08a0a763f523484d74a3a766f86192e3e15a
dohauzi@gmail.com <dohauzi@gmail.com>
2025-02-02 16:50:08 +0900

    폴리필 설정

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,24 @@ module.exports = {
         }),
     ],
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx'],
+        fallback: {
+            // 필요한 Node.js 코어 모듈 폴리필 (브라우저에서 동작 가능한 모듈로)
+            zlib: require.resolve('browserify-zlib'),
+            crypto: require.resolve('crypto-browserify'),
+            vm: require.resolve('vm-browserify'),
+            os: require.resolve('os-browserify/browser'),
+            https: require.resolve('https-browserify'),
+            http: require.resolve('stream-http'),
+            querystring: require.resolve('querystring-es3'),
+            // 사용하지 않는 모듈은 false로 설정
+            fs: false,
+            child_process: false,
+            module: false,
+            worker_threads: false,
+            // path는 Node.js의 path 기능을 브라우저에서 사용할 경우
+            // path-browserify를 사용할 수 있습니다.
+            path: require.resolve('path-browserify')
+        }
     }
 };
