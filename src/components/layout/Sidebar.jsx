845a94fd54beb196793eb1cfd23747b113c453f3
dohauzi@gmail.com <dohauzi@gmail.com>
2025-02-24 12:28:20 +0900

    사이드바 수정

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -21,6 +21,16 @@ const Sidebar = ({
   const selectedClass = 'selected';
   const showContent = isOpen || isLocked;
 
+  // 현재 메뉴가 active한지 판단하는 함수 (부모 혹은 하위 메뉴가 현재 페이지인지)
+  const isMenuActive = (menu) => {
+    if (menu.path === pathName) return true;
+    if (menu.subItems && menu.subItems.some(sub => sub.path === pathName)) return true;
+    return false;
+  };
+
+  // 현재 페이지에 해당하는 메뉴 그룹만 필터링
+  const activeMenus = menuItems.filter(menu => menu.show && isMenuActive(menu));
+
   return (
     <SidebarContainer
       $isLocked={isLocked}
@@ -36,14 +46,35 @@ const Sidebar = ({
           <SidebarContent>
             <h2>HCH</h2>
             <MenuList>
-              {menuItems.map(({ path, label, show }) => (
-                show ?
-                  <MenuItem key={path}>
-                    <StyledNavLink to={path} className={path === pathName ? selectedClass : ''}>
-                      {label}
+              {activeMenus.map(menu => (
+                <React.Fragment key={menu.path}>
+                  {/* 1뎁스 메뉴 - subItems가 있는 경우 selected 스타일 적용 안함 */}
+                  <MenuItem>
+                    <StyledNavLink
+                      to={menu.path}
+                      className={
+                        menu.subItems && menu.subItems.length > 0
+                          ? ''
+                          : (menu.path === pathName ? selectedClass : '')
+                      }
+                    >
+                      {menu.label}
                     </StyledNavLink>
                   </MenuItem>
-                  : ""
+                  {/* subItems가 있다면 항상 표시 */}
+                  {menu.subItems && menu.subItems.map(sub => (
+                    sub.show && (
+                      <MenuItem key={sub.path} style={{ paddingLeft: '20px' }}>
+                        <StyledNavLink
+                          to={sub.path}
+                          className={sub.path === pathName ? selectedClass : ''}
+                        >
+                          {sub.label}
+                        </StyledNavLink>
+                      </MenuItem>
+                    )
+                  ))}
+                </React.Fragment>
               ))}
             </MenuList>
           </SidebarContent>
---
+++ src/components/layout/bak/Sidebar copy.jsx
@@ -0,0 +1,56 @@
+// File: components/Sidebar.jsx
+import React from 'react';
+import {
+  SidebarContainer,
+  LockButton,
+  SidebarContent,
+  MenuList,
+  MenuItem,
+  StyledNavLink
+} from '../style/SidebarStyle';
+import { menuItems } from '@config/menuConfig';
+
+const Sidebar = ({
+  isOpen,
+  isLocked,
+  onMouseEnter,
+  onMouseLeave,
+  onLockToggle,
+  pathName
+}) => {
+  const selectedClass = 'selected';
+  const showContent = isOpen || isLocked;
+
+  return (
+    <SidebarContainer
+      $isLocked={isLocked}
+      $isOpen={isOpen}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {showContent && (
+        <>
+          <LockButton onClick={onLockToggle}>
+            {isLocked ? 'Unlock' : 'Lock'}
+          </LockButton>
+          <SidebarContent>
+            <h2>HCH</h2>
+            <MenuList>
+              {menuItems.map(({ path, label, show }) => (
+                show ?
+                  <MenuItem key={path}>
+                    <StyledNavLink to={path} className={path === pathName ? selectedClass : ''}>
+                      {label}
+                    </StyledNavLink>
+                  </MenuItem>
+                  : ""
+              ))}
+            </MenuList>
+          </SidebarContent>
+        </>
+      )}
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
