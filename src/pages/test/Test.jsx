45f3987fd4afafe491c5ec83ada47934ec08fb13
dohauzi@gmail.com <dohauzi@gmail.com>
2025-04-17 15:37:56 +0900

    헬스체크 버튼 URL 변경

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { send } from '@utils/clientUtil';
 
+import { API_CONFIG, GET_HOST } from '@config/apiConfig'
+
+
 const healthCheckHandler = async (e) => {
 
     console.log("HealthCheck Start");
 
-    const sendUrl = "http://localhost:18080/dart/main/check";
+    // const sendUrl = "http://localhost:18080/dart/main/check";
+    const sendUrl = `${GET_HOST()}${API_CONFIG.TRADE.COMPVALUE.URL}`;
     const { data, error } = await send(sendUrl, {}, "GET");
 
     console.log("HealthCheck Result", data, error);
